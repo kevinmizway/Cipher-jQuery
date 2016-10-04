fd3725d65e44e9282f155026b72154439c8c85c5
Kevin Mizway <mizzledragon@gmail.com>
2016-10-04 03:49:17 -0400

    Create detailed comments in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,22 @@ jQuery(document).ready(function() {
   var ogSentence = prompt("Please type in any sentence");
 
   var capitalizeFirstLastLetters = function() {
-    return (ogSentence.charAt(0) + ogSentence.charAt(ogSentence.length-1)).toUpperCase();
+    return (ogSentence.charAt(0) + ogSentence.charAt(ogSentence.length-1)).toUpperCase(); //Returns first and last letters of original sentence - capitalized
   };
 
   var reverseSentence = function() {
-    return capitalizeFirstLastLetters().split("").reverse().join("");
+    return capitalizeFirstLastLetters().split("").reverse().join(""); //Reverses capitalizedFirstLetters string
   }
 
   var concatOgReverse = function() {
-    return ogSentence + reverseSentence();
+    return ogSentence + reverseSentence(); // concatenates original sentence with new reversed sentence
   }
 
   var newResult = function() {
-    return (concatOgReverse().charAt(concatOgReverse().length / 2)) + concatOgReverse();
+    return (concatOgReverse().charAt(concatOgReverse().length / 2)) + concatOgReverse(); // Returns length of concatOgReverse / 2 and concatenates with concatOgReverse
   }
 
-  var finalCipherResult = newResult().split("").reverse().join("");
+  var finalCipherResult = newResult().split("").reverse().join(""); //Reverses newResult into final cipher
 
   jQuery(".tiger").click(function() {
     alert(ogSentence);
